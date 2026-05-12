64309286f85738941c80c4dd3ca886ed68caa23b
isabellicmelo <isabellicmelo@gmail.com>
2026-05-12 08:09:06 -0300

    subindo  todo oprojeto

---
+++ LoginProjeto/LoginProjeto/Models/Cliente.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginProjeto.Models
+{
+    public class Cliente
+    {
+        /*PC*/
+        [Display(Name = "Código", Description = "Código.")]
+        public int Id { get; set; }
+
+        [Display(Name ="Nome completo", Description ="Nome e Sobrenome.")]
+        [Required(ErrorMessage ="O nome completo é obrigatório.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Nascimento")]
+        [Required(ErrorMessage = "A data é obrigatório.")]
+        public string Nascimento { get; set; }
+
+        [Display(Name ="Sexo")]
+        [Required(ErrorMessage ="O Sexo é obrigatório")]
+        [StringLength(1, ErrorMessage = "Deve conter 1 caracter")]
+
+        public string Sexo {  get; set; }
+
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage = "O CPF é obrigatório")]
+        public string CPF { get; set; }
+
+        [Display(Name = "Celular")]
+        [Required(ErrorMessage = "O Celular é obrigatório")]
+        public string Telefone { get; set; }
+
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "O Email é obrigatório")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido...")]
+        public string Email { get; set; }
+
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "A senha é obrigatório")]
+        [StringLength(10, MinimumLength = 6, ErrorMessage = "A senha deve ter entre 6 e 10 caracteres")]
+        public string Senha { get; set; }
+
+        [Display(Name = "Situação")]
+        [Required(ErrorMessage = "A Situação é obrigatório")]
+        public string Situacao { get; set; }
+    }
+}
---
+++ LoginProjeto/LoginProjeto/Models/Colaborador.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginProjeto.Models
+{
+    public class Colaborador
+    {
+        [Display(Name = "Nome Completo", Description = "Nome e Sobrenome.")]
+        [Required(ErrorMessage = "o nome completo é obrigatorio")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Celular")]
+        [Required(ErrorMessage = "o celular é obrigatorio")]
+        public string Telefone { get; set; }
+
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage = "o CPF é obrigatorio")]
+        public string CPF { get; set; }
+
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "O Email nao é valido")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um Email valido...")]
+        public string Email { get; set; }
+
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "A Senha é obrigatoria")]
+        [StringLength(10, MinimumLength = 6, ErrorMessage = " A Senha deve ter entre 6 e 10 caracteres")]
+        public string Senha { get; set; }
+
+        [Display(Name = "Tipo")]
+        [Required(ErrorMessage = "o Tipo é obrigatorio")]
+        public string Tipo { get; set; }
+    }
+}
